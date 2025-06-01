ce2a3ea4f3ea58e2cc8adf7beb1ffd8512d422a8
rcedenod <rafaelcedeno2004@gmail.com>
2025-06-01 18:22:30 -0400

    logica para valores aleatorios y limpiar matrices

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const sizeSelect = document.getElementById("sizeSelect");
+const selectSize = document.getElementById("selectSize");
 const matrixAContainer = document.getElementById("matrixA");
 const matrixBContainer = document.getElementById("matrixB");
+const randomAButton = document.getElementById("randomA");
+const randomBButton = document.getElementById("randomB");
+const clearAButton = document.getElementById("clearA");
+const clearBButton = document.getElementById("clearB");
 
-let n = parseInt(sizeSelect.value);
+let n = parseInt(selectSize.value);
 let matrixA = [];
 let matrixB = [];
 
@@ -32,12 +36,47 @@ function createMatrixInputs(container, matrixArray) {
     }
 }
 
+// valores aleatorios
+function randomValues(matrixArray, container) {
+    // obtengo todos los inputs
+    const inputs = container.querySelectorAll("input");
+    // por cada input, obtengo su correspondiete indice en el array de matriz
+    // y genero un valor aleatorio
+    inputs.forEach((input, index) => {
+        const i = Math.floor(index / n);
+        const j = index % n;
+        const val = Math.floor(Math.random() * 21) - 10;
+        input.value = val;
+        matrixArray[i][j] = val;
+    });
+}
+
+// limpiar matriz
+function clearMatrix(matrixArray, container) {
+    // obtengo todos los inputs
+    const inputs = container.querySelectorAll("input");
+    // a cada input le asigno 0 y a su posicion en el array de matriz
+    inputs.forEach((input, index) => {
+        input.value = "0";
+        const i = Math.floor(index / n);
+        const j = index % n;
+        matrixArray[i][j] = 0;
+    });
+}
+
 // manejando tamaño de matriz
-sizeSelect.addEventListener("change", () => {
-    n = parseInt(sizeSelect.value);
+selectSize.addEventListener("change", () => {
+    n = parseInt(selectSize.value);
     createMatrixInputs(matrixAContainer, matrixA);
     createMatrixInputs(matrixBContainer, matrixB);
 });
+
+// eventos para los botones random A y random B
+randomAButton.addEventListener("click", () => randomValues(matrixA, matrixAContainer));
+randomBButton.addEventListener("click", () => randomValues(matrixB, matrixBContainer));
+// eventos para limpiar A y limpiar B
+clearAButton.addEventListener("click", () => clearMatrix(matrixA, matrixAContainer));
+clearBButton.addEventListener("click", () => clearMatrix(matrixB, matrixBContainer));
 
 // llamo la funcion para inicializar las dos matrices
 createMatrixInputs(matrixAContainer, matrixA);
